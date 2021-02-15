39f6d75aa9025ff5b9f2a4cd4fbf32e267dbe108
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-02-15 17:23:16 +0300

    [FIR] Fix false-positive smartcast on receiver in rhs of elvis
    #KT-44942 Fixed

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/semantics/IrJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/semantics/IrJsCodegenBoxTestGenerated.java
@@ -25030,6 +25030,11 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
             runTest("compiler/testData/codegen/box/smartCasts/kt44932.kt");
         }
 
+        @TestMetadata("kt44942.kt")
+        public void testKt44942() throws Exception {
+            runTest("compiler/testData/codegen/box/smartCasts/kt44942.kt");
+        }
+
         @TestMetadata("lambdaArgumentWithoutType.kt")
         public void testLambdaArgumentWithoutType() throws Exception {
             runTest("compiler/testData/codegen/box/smartCasts/lambdaArgumentWithoutType.kt");
